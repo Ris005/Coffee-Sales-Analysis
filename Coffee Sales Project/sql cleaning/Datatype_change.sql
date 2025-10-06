1ce05b392795e452bdaae4a969fb901a6433a067
Rishu Anand <95202689+Ris005@users.noreply.github.com>
2025-10-06 13:47:16 +0530

    Add files via upload

---
+++ Coffee Sales Project/sql cleaning/Datatype_change.sql
@@ -0,0 +1,35 @@
+-- create database Coffee_shop_sales_db;
+-- USE coffee_shop_sales_db;
+-- RENAME TABLE `coffee shop sales` TO coffee_shop_sales;
+select * from coffee_shop_sales;
+-- describe coffee_shop_sales;
+
+-- SET SQL_SAFE_UPDATES = 0;
+-- UPDATE coffee_shop_sales
+-- SET transaction_date = STR_TO_DATE(transaction_date, '%d-%m-%Y');
+-- SET SQL_SAFE_UPDATES = 1;
+
+-- ALTER TABLE coffee_shop_sales
+-- MODIFY COLUMN transaction_date DATE;
+
+-- describe coffee_shop_sales;
+
+-- SET SQL_SAFE_UPDATES = 0;
+
+-- UPDATE coffee_shop_sales
+-- SET transaction_time = STR_TO_DATE(transaction_time, '%H:%i:%s');
+
+-- -- Optionally, turn it back on
+-- SET SQL_SAFE_UPDATES = 1;
+
+-- ALTER TABLE coffee_shop_sales
+-- MODIFY COLUMN transaction_time TIME;
+
+-- describe coffee_shop_sales;
+
+-- alter table coffee_shop_sales
+-- change column ï»¿transaction_id transaction_id int;
+
+-- describe coffee_shop_sales;
+
+
